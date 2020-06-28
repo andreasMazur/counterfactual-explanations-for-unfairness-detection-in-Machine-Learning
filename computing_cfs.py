715a632df36200740575f5f70faae0ba52d487a9
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2020-06-28 20:21:53 +0200

    Initial commit on github

---
+++ computing_cfs.py
@@ -0,0 +1,458 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import cvxpy as cp
+import csv
+import math
+
+# The dimension of a vector within our data set
+VECTOR_DIMENSION = 9
+
+
+def round_down(vector_1, model, index):
+
+    if index == len(vector_1):
+        if model.decision_function(np.array([vector_1])) < 0:
+            return vector_1
+        return None
+
+    # copy the vector
+    vector_2 = vector_1.copy()
+
+    # Round in both directions
+    vector_1[index] = math.floor(vector_1[index])
+    vector_2[index] = math.ceil(vector_2[index])
+
+    # Compute the 'cost'
+    cost1 = model.decision_function(np.array([vector_1]))[0]
+    cost2 = model.decision_function(np.array([vector_2]))[0]
+
+    # Decide to which leaf we will expand
+    if cost1 <= cost2:
+        result = round_down(vector_1, model, index+1)
+        if result is None:
+            result = round_down(vector_2, model, index+1)
+            if result is None:
+                return None
+            else:
+                return result
+        else:
+            return result
+    else:
+        result = round_down(vector_2, model, index + 1)
+        if result is None:
+            result = round_down(vector_1, model, index + 1)
+            if result is None:
+                return None
+            else:
+                return result
+        else:
+            return result
+
+
+def round_up(vector_1, model, index):
+    if index == len(vector_1):
+        if model.decision_function(np.array([vector_1])) > 0:
+            return vector_1
+        return None
+
+    # copy the vector
+    vector_2 = vector_1.copy()
+    # Round in both directions
+    vector_1[index] = math.floor(vector_1[index])
+    vector_2[index] = math.ceil(vector_2[index])
+
+    # Compute the 'cost'
+    cost1 = model.decision_function(np.array([vector_1]))[0]
+    cost2 = model.decision_function(np.array([vector_2]))[0]
+
+    # Decide to which leaf we will expand
+    if cost1 <= cost2:
+        result = round_up(vector_2, model, index + 1)
+        if result is None:
+            result = round_up(vector_1, model, index + 1)
+            if result is None:
+                return None
+            else:
+                return result
+        else:
+            return result
+    else:
+        result = round_up(vector_1, model, index + 1)
+        if result is None:
+            result = round_up(vector_2, model, index + 1)
+            if result is None:
+                return None
+            else:
+                return result
+        else:
+            return result
+
+
+def rounding(data, model):
+    """
+    Round the counterfactual explanations to whole integers.
+
+    :param data: 'dict'
+                 A dictionary divided in valid and non-valid counterfactuals with:
+                 - the actual feature vectors x
+                 - their labels y
+                 - their counterfactuals x_cf
+                 - their counterfactuals labels y_cf
+    :param model: 'sklearn.linear_model.LogisticRegression'
+                  The used classifier for predicting the initial
+                  Counterfactual explanations
+    :return: 'numpy.ndarray'
+             The rounded counterfactual explanations
+    """
+    print("Rounding values..")
+
+    # Round each initial counterfactual explanation
+    for sub_dataset in ["valid_cf", "non_valid_cf"]:
+        new_cfs = []
+        for i, x_cf in enumerate(data[sub_dataset]["x_cf"]):
+            if data[sub_dataset]["y_cf"][i] == 1:
+                x_cf = round_up(x_cf, model, 0)
+            else:
+                x_cf = round_down(x_cf, model, 0)
+            new_cfs.append(x_cf)
+        data[sub_dataset]["x_cf"] = new_cfs
+
+    return data
+
+
+def manhatten_dist(x, x_cf):
+    sum = 0
+    for j in range(x.shape[0]):
+        sum += cp.abs(x[j] - x_cf[j])
+    return sum
+
+
+def store_results(result, result_type, file_name):
+    pd.DataFrame(result[result_type]).to_csv(f"{file_name}.csv"
+                                             , index=False, sep=";"
+                                             , quoting=csv.QUOTE_NONE)
+
+
+def in_boundaries(vec):
+    # TODO: Check if the counterfactual computes the other class
+    return (vec[0] >= 0
+            and vec[1] >= 0
+            and -30 <= vec[2] <= 30
+            and 1 <= vec[3] <= 2
+            and 1 <= vec[4] <= 2
+            and vec[5] >= 0
+            and 1 <= vec[6] <= 2
+            and 0 <= vec[7] <= 5
+            and 1 <= vec[8] <= 2)
+
+
+def compute_cf(classifier, x, protected, solver, integer=False):
+    """
+    :param integer: 'bool'
+                    Whether or not the result only contains integer-values.
+    :param solver: The solver, which will solve the optimization problem.
+    :param protected: 'bool'
+                     Whether or not protected attributes should be
+                     held protected
+    :param classifier: 'sklearn.linear_model.LogisticRegression'
+    :param x: 'numpy.ndarray'
+              The instance for which a counterfactuals shall be computed
+    :return: prob.status, (x, y, x_cf, y_cf)
+    """
+
+    # predicts the opposite of the current prediction
+    y = classifier.predict(x.reshape(1, -1))
+    y_target = 1 - y
+
+    # w.l.o.g. y \in {-1,1}
+    if y_target == 0:
+        y_target = -1
+
+    x_cf = cp.Variable(x.shape[0], integer=integer)
+
+    objective = cp.Minimize(manhatten_dist(x, x_cf))
+
+    # Forming constraints like in 3.2
+    weight_vector = classifier.coef_[0]
+    q = -y_target * weight_vector
+    c = -classifier.intercept_ * y_target
+
+    # cvxpy doesn't allow strict inequalities
+    constraints = [q.T @ x_cf + c <= 0]
+
+    # protected constraints
+    if protected:
+        coefficients = np.zeros((VECTOR_DIMENSION, VECTOR_DIMENSION))
+        coefficients[0, 0] = 1  # age
+        # coefficients[3, 3] = 1  # decile_score
+        coefficients[6, 6] = 1  # sex
+        coefficients[7, 7] = 1  # race
+
+        x_constants = np.zeros(VECTOR_DIMENSION)
+        x_constants[0] = x[0]
+        # x_constants[3] = x[3]
+        x_constants[6] = x[6]
+        x_constants[7] = x[7]
+
+        constraints += [coefficients @ x_cf == x_constants]
+
+    # lower bounds
+    lower_bounds = np.zeros((VECTOR_DIMENSION, VECTOR_DIMENSION))
+    for i in [1, 2, 3, 4, 5, 8]:
+        lower_bounds[i, i] = 1
+
+    lb_vector = np.zeros(VECTOR_DIMENSION)
+    for i in [3, 4, 8]:
+        lb_vector[i] = 1
+
+    constraints += [-(lower_bounds @ (x_cf - lb_vector)) <= 0]
+
+    # upper bounds
+    upper_bounds = np.zeros((VECTOR_DIMENSION, VECTOR_DIMENSION))
+    for i in [3, 4, 8]:
+        upper_bounds[i, i] = 1
+
+    ub_vector = np.zeros(VECTOR_DIMENSION)
+    for i in [3, 4, 8]:
+        ub_vector[i] = 2
+
+    constraints += [-(upper_bounds @ (ub_vector - x_cf)) <= 0]
+
+    # Solve the problem
+    prob = cp.Problem(objective, constraints)
+    prob.solve(solver=solver)
+    if prob.status != "optimal" and prob.status != "optimal_inaccurate":
+        raise ValueError("problem is infeasible")
+
+    return x, y[0], x_cf.value, classifier.predict(x_cf.value.reshape(1, -1))[0]
+
+
+def process_data(classifier, data, solver, result_name, integer=False, protected=True):
+    """x.reshape(1, -1)
+    :param integer: 'bool'
+                    Whether or not the result only contains integer-values.
+    :param result_name: 'str'
+                         The identifier for the resulting dictionary.
+    :param solver: The solver, which will solve the optimization problem.
+    :param protected: 'bool'
+                     Whether or not protected attributes will be
+                     held protected
+    :param classifier: 'sklearn.linear_model.LogisticRegression'
+    :param data: 'numpy.ndarray'
+                 The instances, for which counterfactuals will be (if available)
+                 produced.
+    :return: 'dict'
+             The results categorized in three groups:
+             - valid_cf: instances and their found counterfactuals
+             - non_valid_cf: instances and their found counterfactuals, but the values
+                             of the cfs are not valid
+             - no_cf_found: instances, for which no counterfactuals were found.
+    """
+
+    result = {
+        "result_name": result_name,
+        "used_solver": solver,
+        "valid_cf": {"x": [], "y": [], "x_cf": [], "y_cf": []},
+        "non_valid_cf": {"x": [], "y": [], "x_cf": [], "y_cf": []},
+        "no_cf_found": {"x": [], "y": [], "x_cf": [], "y_cf": []},
+        "not_solvable": 0
+    }
+
+    # Rounding values to integers for visualization purposes
+    for i, instance in enumerate(data):
+        try:
+            print(f"{i / len(data) * 100 :.2f}%")
+            x, y, x_cf, y_cf = compute_cf(classifier, instance, protected, solver, integer)
+            if y == y_cf:
+                result["no_cf_found"]["x"].append(list(x))
+                result["no_cf_found"]["y"].append(y)
+                result["no_cf_found"]["x_cf"].append(list(x_cf))
+                result["no_cf_found"]["y_cf"].append(y_cf)
+            elif not in_boundaries(x_cf):
+                result["non_valid_cf"]["x"].append(list(x))
+                result["non_valid_cf"]["y"].append(y)
+                result["non_valid_cf"]["x_cf"].append(list(x_cf))
+                result["non_valid_cf"]["y_cf"].append(y_cf)
+            else:
+                result["valid_cf"]["x"].append(list(x))
+                result["valid_cf"]["y"].append(y)
+                result["valid_cf"]["x_cf"].append(list(x_cf))
+                result["valid_cf"]["y_cf"].append(y_cf)
+        except ValueError:
+            result["not_solvable"] += 1
+
+    return result
+
+
+def get_data():
+    """
+    Reads the 'compas-scores-two-years.csv'-file, filters it with the same conditions
+    that have been used in the propublica-analysis and provides train and test sets for
+    the logistic regression.
+
+    :return: 'numpy.ndarray', 'numpy.ndarray', 'numpy.ndarray', 'numpy.ndarray'
+    """
+
+    # Not filtered data
+    recidivsm_data = pd.read_csv("compas-scores-two-years.csv")
+
+    # Function that filters as in Propublica analysis
+    def filter_as_in_literature(df):
+        newdf = df.filter(items=["age"
+            , "c_charge_degree"
+            , "race"
+            , "score_text"
+            , "sex"
+            , "priors_count"
+            , "days_b_screening_arrest"
+            , "decile_score"
+            , "is_recid"
+            , "two_year_recid"
+            , "c_jail_in"
+            , "c_jail_out"])
+        return newdf.query("days_b_screening_arrest <= 30"
+                           ).query("days_b_screening_arrest >= -30"
+                                   ).query("is_recid != -1"
+                                           ).query("c_charge_degree != 0"
+                                                   ).query('score_text != \'N/A\'')
+
+    # The filtered data
+    filtered_data = filter_as_in_literature(recidivsm_data)
+
+    ###### FEATURE TRANSFORMATION #####
+    def make_labels(df):
+        labels = []
+        for decile_score in np.array(df):
+            if decile_score <= 4:
+                labels.append(0)
+            else:
+                labels.append(1)
+        return labels
+
+    # The Labels: 0 if Low risk, 1 else
+    response_vars = make_labels(filtered_data.filter(items=["decile_score"]))
+
+    # Time in jail in hours
+    jail_in_out = [round((datetime.strptime(jail_out, '%Y-%m-%d %H:%M:%S') - datetime.strptime(jail_in,
+                                                                                         '%Y-%m-%d %H:%M:%S')).total_seconds() / 3600)
+                   for jail_in, jail_out in zip(filtered_data["c_jail_in"], filtered_data["c_jail_out"])]
+
+    # Sex: 1 man, 0 women
+    def categorize(to_compare, options):
+        for i in range(len(options)):
+            if to_compare == options[i]:
+                return i
+
+    sex = [categorize(gender, ["Female", "Male"]) for gender in filtered_data["sex"]]
+
+    ''' Race
+
+    African-American: 0
+    Asian: 1
+    Caucasian: 2
+    Hispanic: 3
+    Native American: 4
+    Other: 5
+
+    '''
+    race = [categorize(r, ["African-American", "Asian", "Caucasian", "Hispanic"
+        , "Native American", "Other"]) for r in filtered_data["race"]]
+
+    # charge degree: F = 0; M = 1;
+    charge_degree = [categorize(charge, ["F", "M"]) for charge in filtered_data["c_charge_degree"]]
+
+    # Final (not normalized) dataset
+    pred_vars = filtered_data.filter(items=["age"
+        , "priors_count"
+        , "days_b_screening_arrest"
+        , "is_recid"
+        , "two_year_recid"])
+    pred_vars["time_in_jail"] = jail_in_out
+    pred_vars["sex"] = sex
+    pred_vars["race"] = race
+    pred_vars["charge_degree"] = charge_degree
+    # print(pred_vars)
+
+    X_train, X_test, y_train, y_test = train_test_split(pred_vars, response_vars
+                                                        , test_size=0.33, random_state=1337)
+
+    # Use numpy arrays for ceml
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    return X_train, X_test, y_train, y_test
+
+
+def main():
+    # read the data from 'compas-scores-two-years'
+    X_train, X_test, y_train, y_test = get_data()
+
+    # train the logistic regression model
+    log_reg = LogisticRegression(max_iter=1500).fit(X_train, y_train)
+
+    # Export the actual vectors
+    all_x = np.concatenate((X_train, X_test))
+    all_y = np.concatenate((y_train, y_test))
+    pd.DataFrame({"x": all_x.tolist(), "y": all_y.tolist()}).to_csv("x_values.csv"
+                                                                    , index=False, sep=";"
+                                                                    , quoting=csv.QUOTE_NONE)
+
+    # List for collecting the results
+    results = []
+
+    # compute counterfactuals
+    result = process_data(log_reg, X_test, cp.CBC, "ILP", True)
+    # As the numeric solution might be not a whole integer but rather something very
+    # close to an integer, we round those values.
+    result["valid_cf"]["x_cf"] = np.round(result["valid_cf"]["x_cf"]).tolist()
+    result["non_valid_cf"]["x_cf"] = np.round(result["non_valid_cf"]["x_cf"]).tolist()
+    results.append(result)
+
+    # compute counterfactuals without protected attributes
+    result_npa = process_data(log_reg, X_test, cp.CBC, "ILP - npa", True, False)
+    result_npa["valid_cf"]["x_cf"] = np.round(result_npa["valid_cf"]["x_cf"]).tolist()
+    result_npa["non_valid_cf"]["x_cf"] = np.round(result_npa["non_valid_cf"]["x_cf"]).tolist()
+    results.append(result_npa)
+
+    # compute counterfactuals with relaxation
+    result_wr = rounding(process_data(log_reg, X_test, cp.SCS, "ILP - wr"), log_reg)
+    results.append(result_wr)
+
+    # compute counterfactuals with relaxation but without protected attributes
+    result_wr_npa = rounding(process_data(log_reg, X_test, cp.SCS, "ILP - wr - npa", False, False), log_reg)
+    results.append(result_wr_npa)
+
+    # export results to csv files for further investigations
+    store_results(result, "valid_cf", "valid_cf")
+    store_results(result_wr, "valid_cf", "valid_cf_wr")
+    store_results(result_npa, "valid_cf", "valid_cf_npa")
+    store_results(result_wr_npa, "valid_cf", "valid_cf_wr_npa")
+
+    store_results(result, "non_valid_cf", "non_valid_cf")
+    store_results(result_wr, "non_valid_cf", "non_valid_cf_wr")
+    store_results(result_npa, "non_valid_cf", "non_valid_cf_npa")
+    store_results(result_wr_npa, "non_valid_cf", "non_valid_cf_wr_npa")
+
+    # report the results
+    print("Computation finished.")
+    print("Accuracy score of the logistic regression:", log_reg.score(X_test, y_test))
+    for res in results:
+        print("\n")
+        print("Experiment:", res["result_name"])
+        print("Used solver:", res["used_solver"])
+        print("Amount of valid counterfactuals", len(res["valid_cf"]["x_cf"]))
+        print("Amount of counterfactuals, that exceed the boundaries:", len(res["non_valid_cf"]["x_cf"]))
+        print("Amount of instances, for which no counterfactual have been found:", len(res["no_cf_found"]["x_cf"]))
+        print("Amount of not optimal or infeasible problems:", res["not_solvable"])
+
+    print("Parameter-values of the logistic regression:")
+    print(f"w = {log_reg.coef_[0]}")
+    print(f"b = {log_reg.intercept_}")
+
+if __name__ == "__main__":
+    main()
---
+++ visualization.py
@@ -0,0 +1,203 @@
+import plotly.graph_objects as go
+import pandas as pd
+import ast
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from scipy.stats import chi2_contingency
+
+# The names in correct order for the columns of a dataset
+categories = ["age", "priors_count", "days_b_screening_arrest", "is_recid"
+    , "two_year_recid", "time_in_jail", "sex", "race", "charge_degree"]
+
+
+def detect_amount_of_changes(xs, x_cfs):
+    """
+    Computes and plots the suggested changes per attribute and group.
+    Subsequently the function plots the results in a histogram.
+
+    :param xs: 'pandas.core.frame.DataFrame'
+           The actual instances from the database.
+    :param x_cfs: 'pandas.core.frame.DataFrame'
+           The counterfactual explanations for the actual instances.
+    :return: y_values: 'pandas.core.frame.DataFrame'
+             The amount of changes per attribute and race.
+    """
+    # Generate sub-datasets for each race
+    sub_datasets_xs = []
+    sub_datasets_x_cfs = []
+    try:
+        groups = list(xs.groupby("race").count().index)
+        for i in groups:
+            truth_values = xs["race"] == i
+            sub_datasets_xs.append(xs[truth_values])
+            # As the race-value might change in the counterfactual
+            # explanation, we take the same truth-values as for xs.
+            sub_datasets_x_cfs.append(x_cfs[truth_values])
+    except IndexError:
+        raise Exception("Check if xs and x_cfs have the correct column descriptions.")
+
+    # Count how many entries are >0 for each column per group
+    y_values = []
+    for (xs_group, x_cfs_group) in zip(sub_datasets_xs, sub_datasets_x_cfs):
+        # Compute the suggested changes for that group
+        delta = x_cfs_group - xs_group
+        amounts = []
+        for column in delta:
+            amounts.append(delta[delta[column] > 0][column].count())
+        y_values.append(amounts)
+
+    # Plot the results in a bar chart
+    group_names = ["African-American", "Asian", "Caucasian", "Hispanic", "Native American", "Other"]
+    bar_chart = go.Figure()
+    for i, y_value in enumerate(y_values):
+        # Add sum description to the last group for each attribute
+        if y_value == y_values[-1]:
+            bar_chart.add_trace(
+                go.Bar(x=categories, y=y_value, name=group_names[i], text=sum(np.array(y_values)),
+                       textposition="auto"))
+        else:
+            bar_chart.add_trace(
+                go.Bar(x=categories, y=y_value, name=group_names[i]))
+
+    bar_chart.update_layout(barmode='relative', title_text='Amount of suggested changes per attribute and race')
+    bar_chart.show()
+
+    # return pd.DataFrame(y_values, index=group_names, columns=categories)
+
+
+def plot_heatmap_for_cramers_v(data, heatmap_name):
+    """
+    Computes the Cramer's V-values for each attribute
+    pair and plots a corresponding heatmap.
+
+    :param data: 'pandas.core.frame.DataFrame'
+           Usually the actual vectors x, for which the attributes dependencies
+           should be computed.
+    :return: chi_2_values: 'pandas.core.frame.DataFrame'
+             A dataframe with the Cramer's V-values for each
+             attribute-pair.
+    """
+    chi_2_values = cramers_v(data)
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.heatmap(chi_2_values, annot=True, cmap=plt.cm.Reds, ax=ax)
+    plt.title("Cramer's V - values")
+    plt.tight_layout()
+    plt.savefig(heatmap_name)
+    plt.show()
+    return chi_2_values
+
+
+def cramers_v(data):
+    """
+    :param data: 'pandas.core.frame.DataFrame'
+           The data for which the chi-2-values should be computed.
+
+    A function to compute the cramers-V-value for each attribute-pair
+    in order to spot relatedness among the features.
+
+    :return cramers_v_table: 'pandas.core.frame.DataFrame'
+            A table with all chi-2-values for each attribute-pair.
+            For example, it can be used to plot heatmaps.
+    """
+
+    # Compute the cramers-V-value for each attribute-pair
+    cramers_v_table = pd.DataFrame(index=data.columns, columns=data.columns)
+
+    # First, compute the chi-2 statistic
+    for i, fst_column in enumerate(data):
+        for j, snd_column in enumerate(data):
+
+            # We just need the upper triangular of the cross-product matrix
+            # without the diagonal because chi-2 is symmetric.
+            if i <= j:
+
+                # Compute all possible value-pairs (cross product)
+                rows_indices = list(data.groupby(fst_column).count().index)
+                column_indices = list(data.groupby(snd_column).count().index)
+                cross_product = [(x, y) for x in rows_indices for y in column_indices]
+                cross_product = np.array(cross_product).reshape(len(rows_indices), len(column_indices), 2)
+
+                # Count how much each value-pair has occurred and put it
+                # into the contingency table.
+                contingency_table = []
+                for row in cross_product:
+                    contingency_table_row = []
+                    for [attr1_value, attr2_value] in row:
+                        cell = data[data[fst_column] == attr1_value]
+                        cell_value = cell[cell[snd_column] == attr2_value].shape[0]
+                        contingency_table_row.append(cell_value)
+                    contingency_table.append(contingency_table_row)
+
+                # Compute the chi-2-value
+                chi_2_value = chi2_contingency(contingency_table)[0]
+
+                # Compute Cramer's v
+                try:
+                    n = data.shape[0]
+                    c = len(column_indices)
+                    r = len(rows_indices)
+                    cramers_v_value = np.sqrt(chi_2_value / (n * min(c - 1, r - 1)))
+
+                    # Add it to the chi-2-table
+                    cramers_v_table.at[fst_column, snd_column] = cramers_v_value
+                    cramers_v_table.at[snd_column, fst_column] = cramers_v_value
+
+                # To compute a
+                except ZeroDivisionError:
+                    print("Contingency table for", fst_column, "and"
+                          , snd_column, "contains structural zeros.")
+                    cramers_v_table.at[fst_column, snd_column] = 0
+                    cramers_v_table.at[snd_column, fst_column] = 0
+                    cramers_v_table.at[fst_column, fst_column] = 1
+
+    return cramers_v_table.apply(pd.to_numeric)
+
+
+def read_data(file_name, skip_cf=False):
+    """
+    :param skip_cf:
+    :param file_name: 'str'
+                      The file from which the data shall be read.
+                      Has to be an output file of the cf-production
+                      program.
+    :param found_cfs: 'bool'
+                      Set to 'False' if you only want the initial
+                      feature vectors without their counterfactuals.
+                      May be usefull for vectors, for which no cf
+                      was found.
+    :return: 'dict'
+             A dictionary
+    """
+
+    data = pd.read_csv(file_name, sep=";")
+    if skip_cf:
+        data_dict = {"x": pd.DataFrame([ast.literal_eval(row) for row in data["x"]], columns=categories),
+                     "y": pd.DataFrame(data["y"])}
+    else:
+        data_dict = {"x": pd.DataFrame([ast.literal_eval(row) for row in data["x"]], columns=categories),
+                     "y": pd.DataFrame(data["y"]),
+                     "x_cf": pd.DataFrame([ast.literal_eval(row) for row in data["x_cf"]], columns=categories),
+                     "y_cf": pd.DataFrame(data["y_cf"])}
+    return data_dict
+
+
+def main():
+    # Retrieve data
+    x_values = read_data("x_values.csv", True)
+    valid_cfs = read_data("valid_cf.csv")
+    valid_cfs_npa = read_data("valid_cf_npa.csv")
+    non_valid_cfs_wr = read_data("non_valid_cf_wr.csv")
+    non_valid_cf_wr_npa = read_data("non_valid_cf_wr_npa.csv")
+
+    # Compute Cramer's V
+    plot_heatmap_for_cramers_v(x_values["x"], "heatmap.png")
+
+    # Detect the amount of suggested changes per attribute
+    detect_amount_of_changes(valid_cfs["x"], valid_cfs["x_cf"])
+    detect_amount_of_changes(valid_cfs_npa["x"], valid_cfs_npa["x_cf"])
+    detect_amount_of_changes(non_valid_cfs_wr["x"], non_valid_cfs_wr["x_cf"])
+    detect_amount_of_changes(non_valid_cf_wr_npa["x"], non_valid_cf_wr_npa["x_cf"])
+
+if __name__ == "__main__":
+    main()
